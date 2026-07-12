3bc61da03e2aae456fc3380729570e39910c48ed
Shreya Jamwal <shreyajamwal19@gmail.com>
2026-07-12 17:26:26 +0530

    nw

--- backend/vibeshelf-backend/src/main/java/com/vibeshelf/vibeshelf_backend/controller/UserController.java
+++ backend/vibeshelf-backend/src/main/java/com/vibeshelf/vibeshelf_backend/controller/UserController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175"})
 public class UserController {
 
     private final UserService userService;
